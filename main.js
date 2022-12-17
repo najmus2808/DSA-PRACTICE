0c8fc7c70553a8cde03a95c0917c763145fccc4e
najmus2808 <najmus2808@gmail.com>
2022-12-18 00:09:24 +0600

    reduce complexity of isElementExists function

--- main.js
+++ main.js
@@ -416,3 +416,30 @@
 
 // ======================
 // VN: 5.4 - end
+
+// VN: 5.5 - start
+// ======================
+
+// Time Complexity: O(n + m)
+// Space Complexity: O(n)
+
+// function isElementExists(arr1, arr2) {
+//   let exists = false;
+//   const frequencyCounter = {};
+//   for (let element of arr1) {
+//     frequencyCounter[element] = true;
+//   }
+
+//   for (let element of arr2) {
+//     if (element in frequencyCounter) {
+//       exists = true;
+//       break;
+//     }
+//   }
+//   return exists;
+// }
+
+// console.log(isElementExists(["a", "b", "c"], [1, 2, 3, "d"]));
+
+// VN: 5.5 - end
+// ======================
