16471d3092ea8ea81eb24fe73ec776aee3f2a552
najmus2808 <najmus2808@gmail.com>
2022-12-20 16:38:32 +0600

    write a funtion for counting vowel

--- main.js
+++ main.js
@@ -643,3 +643,27 @@
 
 // VN: 7.6 - end
 // ===================
+
+// VN: 7.7 - start
+// ===================
+
+function vowelCounter(str) {
+  let count = 0;
+  for (let char of str.toLowerCase()) {
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(vowelCounter("sakib"));
+
+// ===================
+// VN: 7.7 - end
