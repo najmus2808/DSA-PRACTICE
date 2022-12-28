5c8c07fdf1ad78e17fccc36f55439fd6a12c09c5
najmus2808 <najmus2808@gmail.com>
2022-12-28 16:31:10 +0600

    find lengthOfLongestSubstring

--- main.js
+++ main.js
@@ -966,3 +966,33 @@
 
 // VN: 9.5 - end
 // ======================
+
+// VN: 9.6 - start
+// ======================
+// ### 3. Longest Substring Without Repeating Characters (leet code 3)
+// > Given a string s, find the length of the longest substring without repeating characters
+
+// var lengthOfLongestSubstring = function (s) {
+//   let i = 0;
+//   let j = 0;
+//   let max = 0;
+//   const uniqueSet = new Set();
+//   while (i < s.length) {
+//     if (!uniqueSet.has(s[i])) {
+//       uniqueSet.add(s[i]);
+//       max = Math.max(max, uniqueSet.size);
+//       i++;
+//     } else {
+//       uniqueSet.delete(s[j]);
+//       j++;
+//     }
+//   }
+//   return max;
+// };
+
+// console.log(lengthOfLongestSubstring("abcabcbb"));
+// console.log(lengthOfLongestSubstring("bbbbb"));
+// console.log(lengthOfLongestSubstring("pwwkew"));
+
+// VN: 9.6 - end
+// ======================
