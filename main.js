e221021fc7a93bdbb25c64e978cb8bef305ed2a9
najmus2808 <najmus2808@gmail.com>
2022-12-28 17:00:38 +0600

    find minSubArrayLen

--- main.js
+++ main.js
@@ -996,3 +996,33 @@
 
 // VN: 9.6 - end
 // ======================
+
+// VN: 9.7 - start
+// ======================
+// 209. Minimum Size Subarray Sum (leet code 209)
+// > Given an array of positive integers nums and a positive integer target, return the minimal length of a
+// subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+// var minSubArrayLen = function (target, nums) {
+//   let result = Infinity;
+//   let left = 0;
+//   let sum = 0;
+
+//   for (var i = 0; i < nums.length; i++) {
+//     sum += nums[i];
+//     while (sum >= target) {
+//       result = Math.min(result, i + 1 - left);
+//       sum -= nums[left];
+//       left++;
+//     }
+//   }
+//   result = result === Infinity ? 0 : result;
+//   return result;
+// };
+
+// console.log(minSubArrayLen(7, [2, 3, 1, 2, 4, 3]));
+// console.log(minSubArrayLen(4, [1, 4, 4]));
+// console.log(minSubArrayLen(11, [1, 1, 1, 1, 1, 1, 1, 1]));
+
+// VN: 9.7 - end
+// ======================
