947361cf73e834b83edb9aa890a2c87dd3a0ac0d
najmus2808 <najmus2808@gmail.com>
2022-12-28 23:37:10 +0600

    exaple of binary search

--- main1.js
+++ main1.js
@@ -17,3 +17,33 @@
 
 // VN: 10.1 - end
 // ======================
+
+// VN: 10.2 - start
+// ======================
+
+// Binary Search / Divide and Conquer
+
+// function searchEleIndex(array, searchElement) {
+//   let low = 0;
+//   let high = array.length - 1;
+//   while (low <= high) {
+//     let mid = Math.floor((low + high) / 2);
+//     let midValue = array[mid];
+//     if (midValue === searchElement) {
+//       return mid;
+//     }
+//     if (searchElement > midValue) {
+//       low = mid + 1;
+//     } else {
+//       high = mid - 1;
+//     }
+//   }
+//   return -1;
+// }
+
+// console.log(searchEleIndex([1, 2, 3, 4, 5], 4));
+// console.log(searchEleIndex([1, 2, 3, 4, 5], 1));
+// console.log(searchEleIndex([1, 2, 3, 4, 5], 10));
+
+// VN: 10.2 - end
+// ======================
