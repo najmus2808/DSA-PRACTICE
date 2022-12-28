b054bb014176dc7df57b83950cba96987f98c5a3
najmus2808 <najmus2808@gmail.com>
2022-12-28 22:06:50 +0600

    make a searchEleIndex function

---
+++ main1.js
@@ -0,0 +1,19 @@
+// ================== Search Algorithom Start ======================
+// VN: 10.1 - start
+// ======================
+// ### Linear Search
+
+// function searchEleIndex(array, searchElement) {
+//   for (var i = 0; i < array.length; i++) {
+//     if (array[i] === searchElement) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
+
+// console.log(searchEleIndex([1, 2, 3, 4, 5], 4));
+// console.log(searchEleIndex([1, 2, 3, 4, 5], 10));
+
+// VN: 10.1 - end
+// ======================
