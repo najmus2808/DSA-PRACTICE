757ed24fa32ec8652d85ff883b6fbb4646c88051
najmus2808 <najmus2808@gmail.com>
2022-12-29 10:35:37 +0600

    add description

--- main1.js
+++ main1.js
@@ -1,4 +1,9 @@
 // ================== Search Algorithom Start ======================
+
+// Linear Search Approach: A simple approach is to do a linear search. The time complexity of the Linear search is O(n). Another approach to perform the same task is using Binary Search.  
+
+// Binary Search is a searching algorithm used in a sorted array by repeatedly dividing the search interval in half. The idea of binary search is to use the information that the array is sorted and reduce the time complexity to O(Log n). 
+
 // VN: 10.1 - start
 // ======================
 // ### Linear Search
