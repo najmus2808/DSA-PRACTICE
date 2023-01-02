a224ae3f92f69b797ff29bc011b71d1209b9a98f
najmus2808 <najmus2808@gmail.com>
2023-01-02 09:49:27 +0600

    add description

--- main1.js
+++ main1.js
@@ -58,7 +58,7 @@
 
 // VN: 10.4 - start
 // ======================
-// 35. Search Insert Position - (leet code 35)
+// 35. Search Insert Position - (leet code 35) - Submitted
 // Given a sorted array of distinct integers and a target value, return
 // the index if the target is found. If not, return the index where it would
 // be if it were inserted in order.
