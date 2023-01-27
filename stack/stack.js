940944e6b6832a6622ef7157fd3df26dde409028
najmus2808 <najmus2808@gmail.com>
2023-01-27 08:00:36 +0600

    practice with stack

---
+++ stack/stack.js
@@ -0,0 +1,55 @@
+// Stack - Last in Fast out
+// Implementing Stack with Linked List and we choose the biginig of linked list for push node and pop node.
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.top = newNode;
+    this.length = 1;
+  }
+
+  //   adding item to the top of the stack
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.top = newNode;
+    } else {
+      newNode.next = this.top;
+      this.top = newNode;
+    }
+    this.length++;
+    return this; // if you want you can skip this line
+  }
+
+  //   remove item from the top of the stack
+
+  pop() {
+    if (this.length === 0) return null;
+    let temp = this.top;
+    this.top = temp.next;
+    temp.next = null;
+    this.length--;
+    return temp;
+  }
+
+  peek() {
+    const temp = this.pop();
+    this.push(temp);
+    return temp;
+  }
+}
+
+const stack = new Stack(10);
+stack.push(11);
+stack.push(12);
+stack.push(13);
+stack.pop();
+console.log(stack);
+console.log(stack.peek());
