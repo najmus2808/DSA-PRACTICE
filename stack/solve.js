006e68ed1558a1664772daedc38475c8b7a6ab80
najmus2808 <najmus2808@gmail.com>
2023-01-27 10:42:52 +0600

    solve a leetcode problem

---
+++ stack/solve.js
@@ -0,0 +1,31 @@
+// 20. Valid Parentheses - leetcode (20)
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+// An input string is valid if:
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+
+var isValid = function (s) {
+  const stack = [];
+  const pairs = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+
+  for (let char of s) {
+    if (char in pairs) {
+      stack.push(char);
+    } else {
+      if (stack.length === 0) return false;
+      const last = stack.pop();
+      if (pairs[last] !== last) return false;
+    }
+  }
+
+  if (stack.length) {
+    return false;
+  } else {
+    return true;
+  }
+};
